7fbc187c2535d4e78139565b16517428dcc748d3
Alex1Go <125027886+Alex1Go@users.noreply.github.com>
2023-07-04 17:32:22 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import { fetchBreeds, fetchCatByBreed } from './js/cat-api';
-import SlimSelect from 'slim-select';
-
-new SlimSelect({
-  select: '#selectElement'
-})
-select.id = 'selectElement';
+import Notiflix from 'notiflix';
 
 const error = document.querySelector('.error');
 const loader = document.querySelector('.loader');
@@ -14,28 +9,31 @@ const catInfo = document.querySelector('.cat-info');
 select.addEventListener('change', getCatData);
 error.style.display = 'none';
 loader.style.display = 'none';
-
 function getCatData(evt) {
      const catId = evt.target.value;
     fetchCatByBreed(catId)
         .then((data) => {
-         const img = data.url;
-         const description = data.breeds[0].description;
-         const name = data.breeds[0].name;
-         const temperament = data.breeds[0].temperament; 
-         const pageCatInfo = `<img src=${data.url} alt = 'Foto cat' width="400"><div><h2>${data.breeds[0].name}</h2><p>${data.breeds[0].description}</p><p>Temperament: ${data.breeds[0].temperament}</p></div>`;
-            catInfo.innerHTML = pageCatInfo; 
-            loader.style.display = 'block';   
+             loader.style.display = 'block'; 
+            setTimeout(() => {
+                const img = data.url;
+                const description = data.breeds[0].description;
+                const name = data.breeds[0].name;
+                const temperament = data.breeds[0].temperament;
+                const pageCatInfo = `<img src=${data.url} alt = 'Foto cat' width="400"><div><h2>${data.breeds[0].name}</h2><p>${data.breeds[0].description}</p><p>Temperament: ${data.breeds[0].temperament}</p></div>`;
+                catInfo.innerHTML = pageCatInfo;
+                loader.style.display = 'none'; 
+                select.style.marginBottom = '10px'
+            }, 1500);            
         }).catch(() => {
-           error.style.display = 'block';
+            Notiflix.Notify.failure('Ой, щось пішло не так!');
+        //    error.style.display = 'block';
     })
-}
-
+};
 fetchBreeds()
     .then(cats => {
         cats.map(cat => {        
              const option = `<option value ="${cat.id}">${cat.name}</option>`;
-            select.insertAdjacentHTML('beforeend', option);           
+            select.insertAdjacentHTML('beforeend', option);             
         })
     })
     .catch(() => {
@@ -45,3 +43,9 @@ fetchBreeds()
 document.body.style.padding = '20px'
 catInfo.style.display = 'flex';
 catInfo.style.gap = '20px';
+
+
+// import SlimSelect from 'slim-select';
+// new SlimSelect({
+//   select: '.breed-select'
+// })
