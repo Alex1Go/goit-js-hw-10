55632ac528c8e99c84d228d031fd69e5452f2874
Alex1Go <125027886+Alex1Go@users.noreply.github.com>
2023-07-05 17:35:08 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { fetchBreeds, fetchCatByBreed } from './js/cat-api';
 import Notiflix from 'notiflix';
-import SlimSelect from 'slim-select';
-import 'slim-select/dist/slimselect.css';
+// import SlimSelect from 'slim-select';
+// import 'slim-select/dist/slimselect.css';
 
 const error = document.querySelector('.error');
 const loader = document.querySelector('.loader');
@@ -39,12 +39,12 @@ fetchBreeds()
     .then(cats => {
         cats.map(cat => {
             const option = `<option value ="${cat.id}">${cat.name}</option>`;            
-          
+            select.insertAdjacentHTML('beforeend', option);
         });
-          select.insertAdjacentHTML('beforeend', option);
-         new SlimSelect({
-             select: '.breed-select'
-        });   
+        
+        //  new SlimSelect({
+        //      select: '.breed-select'
+        // });   
     })
     .catch(() => {
         Notiflix.Report.failure('Ой, щось пішло не так!', '' );
